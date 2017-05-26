fdbe27a7f91e74d2435d304dc492c92c4be064da
Krlee <515961601@qq.com>
2017-05-26 20:38:09 +0800

    ws

--- resources/views/admin/device/adjust.blade.php
+++ resources/views/admin/device/adjust.blade.php
@@ -489,7 +489,7 @@
             bestTemp = Math.ceil((temps - 0) / 0.7);
             if( bestTemp < 16 ){
                 bestTemp = 16;
-            } else if( bestTemp < 30 ){
+            } else if( bestTemp > 30 ){
                 bestTemp = 30;
             }
 
